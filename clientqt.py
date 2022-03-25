da301fe21d34b1ecf61a4d5808508b2467dc8090
Krafft <skrafft@et.esiea.fr>
2022-03-25 16:41:49 +0100

    Exercice9 finis

--- clientqt.py
+++ clientqt.py
@@ -20,19 +20,19 @@ class MainWindow(QWidget):
     def initUI(self):
         self.setWindowTitle("Client")
         self.setFixedSize(400, 400)
-        self.label1 = QLabel("Enter your host IP:", self)
-        self.text1 = QLineEdit(self)
-        self.text1.move(10, 30)
+        self.label1 = QLabel("Enter your IP:", self)
+        self.ip = QLineEdit(self)
+        self.ip.move(10, 30)
         
         self.label2 = QLabel("Api key:", self)
-        self.text2 = QLineEdit(self)
+        self.api = QLineEdit(self)
         self.label2.move(10,80)
-        self.text2.move(10,100)
+        self.api.move(10,100)
      
         self.label3 = QLabel("Hostname:", self)
-        self.text3 = QLineEdit(self)
+        self.host = QLineEdit(self)
         self.label3.move(10,130)
-        self.text3.move(10,160)
+        self.host.move(10,160)
       
         self.button = QPushButton("Send", self)
         self.button.move(10,190 )
@@ -43,40 +43,44 @@ class MainWindow(QWidget):
         self.show()
 
 
-    def on_click(self):
-        hostname = self.text1.text()
-        api = self.text2.text()
-        ip = self.text3.text()
-
-        if hostname == "":
-            QMessageBox.about(self, "Error", "Please fill the field")
-        else:
-            res = self.__query(hostname,ip , api)
-            if res:
-                self.label2.setText("Answer%s" % (res["Hello"]))
-                self.label2.adjustSize()
-                self.show()
-
+    
     def _create_url(self, res):
-        openstreetmap_url = "https://www.openstreetmap.org/?mlat=<latitude>&mlon=<longitude>#map=12"(
-        res["latitude"],
-        res["longitude"],
-        )
+        openstreetmap_url = "https://www.openstreetmap.org/?mlat=%s&mlon=%s#map=12" % (res["Latitude"],res["Longitude"] )
         return openstreetmap_url
 
     def __query(self, hostname, ip, api):
-        url = "http://%s/ip/%s/geolocation?key=%s" % (hostname, ip, api)
+        url = "http://%s/ip/%s?key=%s" % (hostname, ip, api)
         r = requests.get(url)
         if r.status_code == requests.codes.NOT_FOUND:
             QMessageBox.about(self, "Error", "IP not found")
         if r.status_code == requests.codes.OK:
             return r.json()
-
-
     
+    def on_click(self):
+        hostname = self.host.text()
+        api = self.api.text()
+        ip = self.ip.text()
+
+        if hostname == "" or ip =="" or api =="":
+            QMessageBox.about(self, "Error", "Please fill the field")
+        else:
+            res = self.__query(hostname,ip , api)
+            if res:
+                self.label2.setText("Hello")
+                self.label2.adjustSize()
+                self.show()
+                Urls = main._create_url(res)
+                QDesktopServices.openUrl(QUrl(Urls))
+
+
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main = MainWindow()
+   
     app.exec_()
+   
+    
+
+
